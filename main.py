a2ebdfb2910a1c57214298cfc62b9e15aa7407e1
Kevin7744 <arapbiisubmissions@gmail.com>
2024-04-28 14:54:21 +0300

    file and folder updates

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain_openai.chat_models import ChatOpenAI
+
+load_dotenv()
+
+
+# Create a langchain agent
+
+# Define graph state
+
+# Define the nodes
+
+# Define the graph
+
